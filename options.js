ae36dda131b0c5b8b9ee3137e1bd457e3a412bf8
Matt Bongiovi <mrb2ej@virginia.edu>
2013-11-17 12:25:34 -0500

    Updated options to include account information login

--- options.js
+++ options.js
@@ -1,7 +1,9 @@
 txtPassword = document.getElementById("txtPassword");
+txtUsername = document.getElementById("txtUsername");
+pswPassword = document.getElementById("pswPassword");
 btnSave = document.getElementById("btnSave");
 
 document.getElementById("btnSave").onclick = function(){
 	localStorage['api_key'] = document.getElementById("txtPassword").value;
-	alert("Saved Successfully!");
+	alert("Account Information Saved Successfully!");
 }
